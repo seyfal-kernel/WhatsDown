39518927d6df1435ef367cf172fcb514d10cbb8a
adbenitez <asieldbenitez@gmail.com>
2023-08-15 11:54:13 +0200

    improve mailto Intent handling in ShareActivity

--- src/org/thoughtcrime/securesms/ShareActivity.java
+++ src/org/thoughtcrime/securesms/ShareActivity.java
@@ -41,6 +41,7 @@ import org.thoughtcrime.securesms.permissions.Permissions;
 import org.thoughtcrime.securesms.util.DynamicLanguage;
 import org.thoughtcrime.securesms.util.DynamicNoActionBarTheme;
 import org.thoughtcrime.securesms.util.DynamicTheme;
+import org.thoughtcrime.securesms.util.MailtoUtil;
 import org.thoughtcrime.securesms.util.MediaUtil;
 import org.thoughtcrime.securesms.util.RelayUtil;
 
@@ -126,7 +127,16 @@ public class ShareActivity extends PassphraseRequiredActionBarActivity implement
       streamExtras = getIntent().getParcelableArrayListExtra(Intent.EXTRA_STREAM);
     } else {
         Uri uri = getIntent().getData();
-        if (uri != null) {
+        if (MailtoUtil.isMailto(uri)) {
+            String[] extraEmail = getIntent().getStringArrayExtra(Intent.EXTRA_EMAIL);
+            if (extraEmail == null || extraEmail.length == 0) {
+                getIntent().putExtra(Intent.EXTRA_EMAIL, MailtoUtil.getRecipients(uri));
+            }
+            String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            if (text == null || text.isEmpty()) {
+                getIntent().putExtra(Intent.EXTRA_TEXT, MailtoUtil.getText(uri));
+            }
+        } else if (uri != null) {
             streamExtras.add(uri);
         }
     }
