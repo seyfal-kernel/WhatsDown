6a7caa638acdb599760c0eec1dff4fb93dc4d12b
adbenitez <asieldbenitez@gmail.com>
2023-08-29 08:39:21 +0200

    add public-bots bot

--- src/org/thoughtcrime/securesms/ConversationListActivity.java
+++ src/org/thoughtcrime/securesms/ConversationListActivity.java
@@ -357,7 +357,10 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
         startActivity(new Intent(this, ProfileActivity.class));
         return true;
       case R.id.menu_webxdc_apps_store:
-        handleShowAppStore();
+          handleShowBot("mini-apps@hispanilandia.net", "OPENPGP4FPR:3CC3726E55E69CF4B52368C411819C7E7639B38C#a=mini%2Dapps%40hispanilandia.net&n=&i=jHGRY-9E7jd&s=cRh0KZJmfKJ");
+        return true;
+      case R.id.menu_public_bots:
+          handleShowBot("public-bots@hispanilandia.net", "OPENPGP4FPR:A0DCC2FD2D39D5F4CABE2E7AA5FAFF66B7B9AF7D#a=public%2Dbots%40hispanilandia.net&n=Public%20Bots&i=1DiZsGwxyzv&s=tIEcS-8zZJN");
         return true;
     }
 
@@ -391,15 +394,14 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
       startActivity(intent);
   }
 
-  private void handleShowAppStore() {
-      final String addr = "mini-apps@hispanilandia.net";
+  private void handleShowBot(String addr, String qrdata) {
       final DcContext dcContext = DcHelper.getContext(this);
       int contactId = dcContext.lookupContactIdByAddr(addr);
       if (contactId!=0 && dcContext.getChatIdByContactId(contactId)!=0) {
           openConversation(dcContext.createChatByContactId(contactId), -1);
       } else {
           QrCodeHandler qrCodeHandler = new QrCodeHandler(this);
-          qrCodeHandler.handleQrData("OPENPGP4FPR:3CC3726E55E69CF4B52368C411819C7E7639B38C#a=mini%2Dapps%40hispanilandia.net&n=&i=jHGRY-9E7jd&s=cRh0KZJmfKJ");
+          qrCodeHandler.handleQrData(qrdata);
       }
   }
 
