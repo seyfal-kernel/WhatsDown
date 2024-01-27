a7d08328ad662c31d635dbde944d2d5d169b651d
adbenitez <asieldbenitez@gmail.com>
2024-01-26 21:41:55 -0500

    show address if no last-seen info is available

--- src/org/thoughtcrime/securesms/ConversationTitleView.java
+++ src/org/thoughtcrime/securesms/ConversationTitleView.java
@@ -119,7 +119,9 @@ public class ConversationTitleView extends RelativeLayout {
           subtitleStr = context.getString(R.string.online);
         } else {
           long timestamp = dcContact.getLastSeen();
-          if (timestamp != 0) {
+          if (timestamp == 0) {
+            subtitleStr = dcContact.getAddr();
+          } else {
             Locale locale = DynamicLanguage.getSelectedLocale(context);
             subtitleStr = context.getString(R.string.last_seen_at, DateUtils.getExtendedTimeSpanString(context, locale, timestamp));
           }
