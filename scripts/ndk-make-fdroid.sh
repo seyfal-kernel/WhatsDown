21d6c3c3b86541403ed973b1cae2ce897c93d0fd
adbenitez <asieldbenitez@gmail.com>
2024-08-29 02:07:28 +0200

    fix bug in scripts/ndk-make-fdroid.sh, also rename all vars to uppercase

--- scripts/ndk-make-fdroid.sh
+++ scripts/ndk-make-fdroid.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-rustup=$1
-ndk=$2
-arch=$3
-case "$arch" in
-    "armeabi-v7a") target=armv7-linux-androideabi;;
-    "arm64-v8a") target=aarch64-linux-android;;
-    "x86") target=i686-linux-android;;
-    "x86_64") target=x86_64-linux-android;;
+RUSTUP=$1
+NDK=$2
+ARCH=$3
+case "$ARCH" in
+    "armeabi-v7a") TARGET=armv7-linux-androideabi;;
+    "arm64-v8a") TARGET=aarch64-linux-android;;
+    "x86") TARGET=i686-linux-android;;
+    "x86_64") TARGET=x86_64-linux-android;;
 esac
 
-fdroiddir=chat.delta.lite
-rootdir=/home/runner/work/android
-repo="$rootdir/android"
+FDROIDDIR=chat.delta.lite
+ROOTDIR=/home/runner/work/android
+REPO="$ROOTDIR/android"
 
-mkdir -p "$rootdir"
+mkdir -p "$ROOTDIR"
 cd ..
-mv "$fdroiddir" "$repo"
-pushd "$repo"
+mv "$FDROIDDIR" "$REPO"
+pushd "$REPO"
 
-"$rustup/rustup-init.sh" -y --default-toolchain $(cat scripts/rust-toolchain) --target $target
+"$RUSTUP/rustup-init.sh" -y --default-toolchain $(cat scripts/rust-toolchain) --target $TARGET
 source "$HOME/.cargo/env"
 export PATH="$PATH:$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/"
 export ANDROID_NDK_ROOT="$NDK"
-scripts/ndk-make.sh $arch
+scripts/ndk-make.sh $ARCH
 
 popd
-mv $repo $fdroiddir
+mv $REPO $FDROIDDIR
