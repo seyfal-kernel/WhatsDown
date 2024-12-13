3a5f9b3fed5727cb82a0fa4b6409f0946ab68735
adbenitez <asieldbenitez@gmail.com>
2024-12-13 22:48:31 +0100

    update app name to avoid confusion in f-droid

--- scripts/undo_rebrand.sh
+++ scripts/undo_rebrand.sh
@@ -4,3 +4,4 @@ find ./src/main/assets/help/ -type f -name '*.html' | xargs sed -i 's/github.com
 find ./src/main/assets/help/ -type f -name '*.html' | xargs sed -i 's/ArcaneChat/Delta Chat/g'
 find ./src/ -type f -name 'strings.xml' | xargs sed -i 's/github.com\/ArcaneChat/get.delta.chat/g'
 find ./src/ -type f -name 'strings.xml' | xargs sed -i 's/ArcaneChat/Delta Chat/g'
+sed -i 's/>Delta Chat</>ArcaneChat</g' ./src/main/res/values/strings.xml
